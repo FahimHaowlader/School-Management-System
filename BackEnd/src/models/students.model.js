f2fe02ea69fa84a1e70d63781f5d1316ff3b48b7
Fahim083 <fahimhaowlader07@gmail.com>
2025-10-03 21:36:18 +0600

    students model schema done

---
+++ BackEnd/src/models/students.model.js
@@ -0,0 +1,205 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const studentSchema = new mongoose.Schema(
+  {
+    studentId: {
+      type: String,
+      required: [true, "Student ID is required"],
+      unique: true,
+      trim: true,
+      match: [/^\d{9}$/, "Student ID must be exactly 9 digits"], // exactly 9 digits
+      validate: [
+        {
+          validator: function (value) {
+            const firstFour = parseInt(value.slice(0, 4), 10);
+            return firstFour >= 2000; // first 4 digits cannot be less than 2000
+          },
+          message: "Invalid Student ID",
+        },
+        {
+          validator: function (value) {
+            const twoDigits56 = parseInt(value.slice(4, 6), 10); // digits 5 & 6
+            return twoDigits56 <= 12;
+          },
+          message: "Invalid Student ID",
+        },
+      ],
+    },
+    prefixName: {
+      type: String,
+      trim: true, // remove leading/trailing spaces
+      minlength: [1, "Prefix must be at least 1 character"],
+      maxlength: [10, "Prefix cannot exceed 10 characters"],
+    },
+    firstName: {
+      type: String,
+      required: [true, "First name is required"],
+      trim: true,
+      minlength: [2, "First name must be at least 2 characters"],
+      maxlength: [15, "First name cannot exceed 15 characters"],
+      match: [/^[A-Za-z]+$/, "First name can only contain letters"],
+    },
+
+    middleName: {
+      type: String,
+      required: [true, "Middle name is required"],
+      trim: true,
+      minlength: [2, "Middle name must be at least 2 characters"],
+      maxlength: [15, "Middle name cannot exceed 15 characters"],
+      match: [/^[A-Za-z]+$/, "Middle name can only contain letters"],
+    },
+
+    lastName: {
+      type: String,
+      trim: true,
+      minlength: [2, "Last name must be at least 2 characters"],
+      maxlength: [15, "Last name cannot exceed 15 characters"],
+      match: [/^[A-Za-z]+$/, "Last name can only contain letters"],
+    },
+
+    dateOfBirth: {
+      type: Date,
+      set: (value) => new Date(value), // Convert string → Date
+      validate: {
+        validator: function (value) {
+          // Check if the date is valid
+          if (isNaN(value.getTime())) return false;
+
+          const today = new Date();
+          const minDate = new Date(
+            today.getFullYear() - 25,
+            today.getMonth(),
+            today.getDate()
+          ); // Max 25 years old
+          const maxDate = new Date(
+            today.getFullYear() - 4,
+            today.getMonth(),
+            today.getDate()
+          ); // Min 4 years old
+
+          return value >= minDate && value <= maxDate;
+        },
+        message: "Invalid DOB. Age must be between 4 and 25 years.",
+      },
+    },
+
+    bloodGroup: {
+      type: String,
+      enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"], // allowed blood types
+      trim: true, // remove extra spaces
+    },
+
+    address: {
+      type: String,
+      trim: true, // removes leading/trailing spaces
+      minlength: [5, "Address must be at least 5 characters"],
+      maxlength: [200, "Address cannot exceed 200 characters"],
+    },
+    phoneNumber: {
+      type: String,
+      required: [true, "Phone number is required"],
+      trim: true,
+      match: [/^[0-9]{11}$/, "Phone number must be exactly 11 digits"],
+    },
+
+    mother:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Guardian",
+         required: true
+        },
+    father:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Guardian",
+            required: true
+        },
+    guardian: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Guardian",
+        default: null
+    },
+
+    pic: {
+      type: String,
+      trim: true,
+      match: [/^(https?:\/\/.*\.(?:png|jpg|jpeg|webp))$/, "Invalid image URL"],
+      default: null,
+    },
+    scholarship: {
+      type: Number,
+      default: 0, // 0 means no scholarship
+      min: [0, "Scholarship cannot be negative"],
+      max: [100, "Scholarship cannot exceed 100"],
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive", "suspended", "transferred", "passout"], // allowed statuses
+      default: "active",
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [8, "Password must be at least 8 characters"],
+      select: false, // Exclude password from queries by default to get this we have to do it like .select (+password)
+    },
+    accountType: {
+      type: String,
+      enum: ["student"], // only "student" allowed for now
+      default: "student",
+      immutable: true, // prevents changing after creation
+      trim: true,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"], // allowed values
+      required: [true, "Gender is required"],
+      trim: true,
+    },
+    admittedAt: {
+      type: Date,
+      required: [true, "Admission date is required"],
+      set: (value) => new Date(value), // Convert string → Date automatically
+    },
+    classes: [
+      {
+        classId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Class",
+          required: true,
+        },
+        result: {
+          type: String,
+          enum: ["A+", "A", "A-", "B", "C", "D", "F"],
+        },
+        promotedToNextClass: { 
+            type: Boolean,
+             required: true,
+             default: false 
+        },
+      },
+    ],
+    leavedAt: {
+      type: Date,
+      set: (value) => new Date(value), // Convert string → Date
+      validate: {
+        validator: function (value) {
+          if (!value) return true; // allow empty (student may not have left yet)
+          if (isNaN(value.getTime())) return false; // invalid date
+          return !this.admittedAt || value >= this.admittedAt; // must be after admission
+        },
+        message: "Leave date cannot be before admission date",
+      },
+    },
+
+    refreshToken: {
+      type: String,
+      select: false, // Exclude refreshToken from queries by default
+    },
+  },
+  { timestamps: true }
+);
+
+
+export const Student = mongoose.model("Student", studentSchema);
