c0a29d9b933cea5062131d2904bb3cc54d6c0008
Fahim083 <fahimhaowlader07@gmail.com>
2025-10-05 18:07:53 +0600

    achievement model done

---
+++ BackEnd/src/models/achievement.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const achievementSchema = new mongoose.Schema(
+  {
+    studentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+      required: [true, "Student ID is required"],
+    },
+    eventId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: [true, "Event ID is required"],
+    },
+    position: {
+      type: String,
+      enum: ["first", "second", "third", "participant"],
+      default: "participant",
+      trim: true,
+    },
+    title: {
+      type: String,
+      required: [true, "Achievement title is required"],
+      trim: true,
+      minlength: [3, "Title must be at least 3 characters"],
+      maxlength: [100, "Title cannot exceed 100 characters"],
+    },
+    description: {
+      type: String,
+      trim: true,
+      maxlength: [500, "Description cannot exceed 500 characters"],
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Achievement = mongoose.model("Achievement", achievementSchema);
