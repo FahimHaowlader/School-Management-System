cca53d6e1ae37846de16d240ea5c644f9946088d
Fahim083 <fahimhaowlader07@gmail.com>
2026-05-04 00:49:39 +0600

    Frontend setup for done for users

---
+++ FrontEnd/Website/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Hello from '@Global/Hello'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Hello />
+<p>hello website</p>
+    </>
+  )
+}
+
+export default App
